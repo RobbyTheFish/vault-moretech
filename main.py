362e1210bce317e46dcad6e10d21df5cd79cbde0
s1lver29 <nikitaselver@yandex.ru>
2024-10-18 04:07:56 +0300

    Update .gitignore and plug for loguru

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import uvicorn
 from api.api import app
+from loguru import logger
+
 
 if __name__ == "__main__":
+    # logger.remove()
+    # logger.add(
+    #     "logs/debug.log",
+    #     format="[{time:YYYY-MM-DD HH:mm:ss}] {level} | {message}",
+    #     level="TRACE",
+    # )
+    # logger.add(
+    #     sys.__stdout__,
+    #     format="[{time:YYYY-MM-DD HH:mm:ss}] {level} | {message}",
+    #     level="TRACE",
+    #     colorize=True,
+    # )
     # Запуск приложения FastAPI
     uvicorn.run(app, host="0.0.0.0", port=8000)
